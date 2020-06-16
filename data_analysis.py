71fcc8af653a73d90f4a8a5c5e1bbc8ea67d209f
siri.surab <siri.surab@gmail.com>
2020-06-16 17:36:27 -0500

    upd data analysis

--- data_analysis.py
+++ data_analysis.py
@@ -1,7 +1,17 @@
-# data analysis code
+import os
+import csv
 
+cereal_csv_path = os.path.join("..", "Resources", "cereal_bonus.csv")
 
-# TODO
+with open(cereal_csv_path) as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=",")
 
+    # @NOTE: This time, we do not use `next(csv_reader)` because there is no header for this file
 
-# TODO
+    # Read through each row of data after the header
+    for row in csv_reader:
+
+        # Convert row to float and compare to grams of fiber
+        if float(row[7]) >= 5:
+            print(row)
+
